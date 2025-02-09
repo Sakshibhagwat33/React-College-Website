80b98e9712cd62352c9a89d2063a1947a4321a41
Sakshibhagwat33 <sakshibhagwat3324@gmail.com>
2025-02-10 00:39:04 +0530

    Initial commit

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -101,7 +101,7 @@ width="100%"
 height="450"
 frameBorder="0"
 style={{ border: 0 }}
-src="https://www.google.com/maps/embed/v1/place?q=college&key=YOUR_GOOGLE_MAPS_API_KEY"
+src="https://www.google.com/maps/embed/v1/plce?q=college&key=YOUR_GOOGLE_MAPS_API_KEY"
 allowFullScreen
 ></iframe>
 
