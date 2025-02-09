41dd096ab939d9c3b445315eff34b039ae4a1cde
Sakshibhagwat33 <sakshibhagwat3324@gmail.com>
2025-02-10 00:12:46 +0530

    Initial commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <div className="container-fluid">
         <div className="row">
           <div className="col-md-4 column" style={{ backgroundColor: "#FFD54F" }}>
-            <h2>⚡ INCREASE YOUR POWER OF ATTENTION, AND MINDFULNESS</h2>
+            <h2>⚡ INCREASE YOUR POWER OF ATTENTION , AND MINDFULNESS</h2>
             <button className="btn btn-outline-light">MORE INFO</button>
           </div>
           <div className="col-md-4 column" style={{ backgroundColor: "#FF6F00" }}>
@@ -20,7 +20,7 @@ function Footer() {
           </div>
         </div>
     <footer className="footer">
-      <p>&copy; 2025 DPU CAMPUS. All rights reserved.</p>
+      <p>&copy; 2025 DPU CAMPUS. | Education University. </p>
       <div className="social-icons">
         <a href="#" target="_blank" rel="noopener noreferrer">Facebook</a>
         <a href="#" target="_blank" rel="noopener noreferrer">Instagram</a>
--- src/component/Header.js
+++ src/component/Header.js
@@ -2,12 +2,46 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
-
 function Header() {
   console.log("Header component rendered.");
   return (
+    
     <header className="header">
-      <div className="logo">
+       <div className="container-fluid d-flex justify-content-between align-items-center p-3">
+          <div className="logo d-flex align-items-center">
+            <div className="logo-box">DPU</div>
+            <div className="logo-text">
+              <h2>DY PATIL</h2>
+              <p>PUNE UNIVERSITY</p>  
+            </div>
+          </div>
+          <div className="contact">
+            <p>Speak with specialist: <span className="phone">888-999-0000</span></p>
+          </div>
+          <div className="social-links">
+            <button className="header-btn">MAIL US</button>
+            <button className="header-btn">OUR LOCATION</button>
+            <div className="icons">
+              <i className="fab fa-facebook"></i>
+              <i className="fab fa-twitter"></i>
+              <i className="fab fa-youtube"></i>
+              <i className="fab fa-google"></i>
+            </div>
+          </div>
+        </div>
+        <nav class="nav-links">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/admissions">Admissions</Link></li>
+          <li><Link to="/programs">Academic Programs</Link></li>
+          <li><Link to="/campus-life">Campus Life</Link></li>
+          <li><Link to="/news">News & Events</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
+          <li><Link to="/login">Sign Up / Log In</Link></li>
+        </ul>
+      </nav>
+      {/* <div className="logo">
         <img src="logo.jpg.png" alt="College Logo" />
       </div>
       <nav>
@@ -21,7 +55,7 @@ function Header() {
           <li><Link to="/contact">Contact Us</Link></li>
           <li><Link to="/login">Sign Up / Log In</Link></li>
         </ul>
-      </nav>
+      </nav> */}
     </header>
   );
 }
