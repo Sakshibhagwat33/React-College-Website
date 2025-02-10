30a7c554e1902ede780e9674fb70a91e33fc3b05
Sakshibhagwat33 <sakshibhagwat3324@gmail.com>
2025-02-10 20:17:27 +0530

    Initial commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -9,7 +9,7 @@ function Header() {
     <header className="header">
        <div className="container-fluid d-flex justify-content-between align-items-center p-3">
           <div className="logo d-flex align-items-center">
-            <div className="logo-box">DPU</div>
+            <div className="logo-box"><img src="logo.jpg.png" alt="logo"></img></div>
             <div className="logo-text">
               <h2>DY PATIL</h2>
               <p>PUNE UNIVERSITY</p>  
@@ -41,21 +41,6 @@ function Header() {
           <li><Link to="/login">Sign Up / Log In</Link></li>
         </ul>
       </nav>
-      {/* <div className="logo">
-        <img src="logo.jpg.png" alt="College Logo" />
-      </div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/admissions">Admissions</Link></li>
-          <li><Link to="/programs">Academic Programs</Link></li>
-          <li><Link to="/campus-life">Campus Life</Link></li>
-          <li><Link to="/news">News & Events</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
-          <li><Link to="/login">Sign Up / Log In</Link></li>
-        </ul>
-      </nav> */}
     </header>
   );
 }
